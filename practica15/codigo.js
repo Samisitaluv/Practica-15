85f887f6ebb1b9e9a1f24eb1bec51dccace83649
Samisitaluv <samayaladuran@gmail.com>
2024-06-04 19:17:27 -0600

    Delete practica15 directory

--- practica15/codigo.js
+++
@@ -1,9 +0,0 @@
-function calcula_densidad() {
-
-    var masa = 5.97E24;
-    var radio = 6378e3;
-    var volumen = (4 / 3) * Math.PI * radio * radio * radio;
-    var densidad = masa / volumen;
-    var resultado = document.getElementById("resultadoA");
-    resultado.innerHTML = densidad.toFixed(2) + ' kg/(metro cúbico)';
-}
